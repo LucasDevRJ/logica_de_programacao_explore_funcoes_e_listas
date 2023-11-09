244f32357362df197666f86676171e326f4e84b9
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-09 20:29:53 -0300

    terceiro desafio concluído

---
+++ desafio_3/desafio.js
@@ -0,0 +1,10 @@
+let listaGenerica = [];
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+
+let outrasLinguagens = ['ASP', 'ActionScript', 'C#'];
+console.log(outrasLinguagens[0]);
+console.log(outrasLinguagens[1]);
+console.log(outrasLinguagens[2]);
